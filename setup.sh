054dbd07be2b9d0143f6d0e07ff1e8710d20f1b8
Roselyn Sunil <roselyn.miriamsunil2024@vitstudent.ac.in>
2026-02-14 12:37:03 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\email=\"roselynmsunil@gmail.com\"\n\
-">~/.streamlit/credentials.toml
-echo "\[server]\n\
-headless=true\n\
-enableCORS=false\n\
-port=$PORT\n\
-">~/.streamlit/config.toml
